fbbd73e0f061758c4ec5f18eb5a15b4b6569b8bb
Hao Huang <hhuang@dataminr.com>
2016-09-06 09:07:04 -0400

    app/objects.js

--- app/async.js
+++ app/async.js
@@ -1,11 +1,15 @@
 exports = (typeof window === 'undefined') ? global : window;
-
 exports.asyncAnswers = {
   async : function(value) {
-
+    return Promise.resolve(value);
   },
 
   manipulateRemoteData : function(url) {
-
+    var req = this.async($.ajax(url));
+    var _this = this;
+    return req.then(function(resp) {
+        var names = resp.people.map(function(v) {return v.name;});
+        return _this.async(names.sort());
+    });  
   }
 };
--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,18 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.objectsAnswers =  {
   alterContext : function(fn, obj) {
-
+    return fn.bind(obj)();
   },
 
   alterObjects : function(constructor, greeting) {
-
+    //Mutating the constructor is not cool tho
+    constructor.prototype.greeting = greeting;
   },
 
   iterate : function(obj) {
-
+    return _.reduce(obj, function(result, v, k) {
+        if(obj.hasOwnProperty(k)) result.push('' + k + ': ' + v);
+        return result;            
+    }, []);
   }
 };
